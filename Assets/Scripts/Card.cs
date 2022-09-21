efce8f45994515c47efb562037473344f50f394c
Stove414 <slucini@clarku.edu>
2022-09-20 20:56:14 -0400

    Directory Fix

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    [SerializeField] private CardInfo[] deckOfCards;
+    private CardInfo temporary;
+
+    void Start()
+    {
+        Shuffle();
+        DisplayOrder();
+    }
+
+    public void Shuffle()
+    {
+        for (int i = 0; i < deckOfCards.Length - 1; i++)
+        {
+            int placement = Random.Range(i, deckOfCards.Length);
+            temporary = deckOfCards[placement];
+            deckOfCards[placement] = deckOfCards[i];
+            deckOfCards[i] = temporary;
+        }
+    }
+
+    public void DisplayOrder()
+    {
+        for (int i = 0; i < deckOfCards.Length; i++)
+        {
+            Debug.Log(deckOfCards[i].name);
+        }
+    }
+}
+
+[System.Serializable]
+public class CardInfo
+{
+    public string name;
+    public string className;
+    public int energyCost;
+    public int attack;
+    public int defense;
+}
