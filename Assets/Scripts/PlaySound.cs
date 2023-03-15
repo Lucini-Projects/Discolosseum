63163a7ed127f75ff493c794147af8844512bfc0
Stove414 <slucini@clarku.edu>
2023-03-14 22:33:33 -0400

    Title Screen Changes

---
+++ Assets/Scripts/PlaySound.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+    //Sound Effect
+    public AudioClip meowSound;
+
+    //Time between meows
+    public int interval = 0;
+
+    
+    bool alreadyMeowing;
+
+    void Update()
+    {
+        if(!alreadyMeowing)
+        {
+            StartCoroutine(Meow());
+        }
+    }
+
+    IEnumerator Meow()
+    {
+        alreadyMeowing = !alreadyMeowing;
+        GetComponent<AudioSource>().clip = meowSound;
+        GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(interval);
+        alreadyMeowing = !alreadyMeowing;
+    }
+}
